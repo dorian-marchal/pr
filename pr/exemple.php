00ad0a26d3ddc8f05538cf1b4fea637cd5c2225e
dorian-marchal <marchal.dorian@gmail.com>
2014-12-10 17:52:50 +0100

    Améliore les exemples de pr

--- pr/exemple.php
+++ pr/exemple.php
@@ -5,6 +5,7 @@ require_once('pr.php');
 // Booléens
 $bool = true;
 pr($bool);
+pr(true && false);
 
 // Entiers / Flottants
 $int = 4;
@@ -15,19 +16,23 @@ pr($float);
 // Chaînes de caractères
 $string = 'test';
 pr($string);
-pr("Chaîne de $string sans variable");
 
 // On passe une ligne
 pr();
 
+// Chaîne simple (non wrappée dans une expression)
+pr("Chaîne de $string sans variable");
+
+pr();
+
 // Avec une fonction
-function ftest($e) {
+function fonctionDeTest($e) {
     return $e;
 }
-pr(ftest("fonction"));
+pr(fonctionDeTest("fonction"));
 
 // Concaténation et somme
-pr(ftest("debut") . ' ' . $string);
+pr(fonctionDeTest("debut") . ' ' . $string);
 pr($int + $float);
 
 pr();
@@ -40,9 +45,9 @@ pr($deux);pr($aie);
 pr();
 
 //Objets et tableaux
-$date = new DateTime('2012-12-21', new DateTimeZone('Europe/Paris'));
-$array = array($string, $int, $float, $date);
-pr($date);
-pr($array);
+$myObject = new DateTime('2012-12-21', new DateTimeZone('Europe/Paris'));
+$myArray = array($string, $int, $float, $myObject);
+pr($myObject);
+pr($myArray);
 
 ?>
