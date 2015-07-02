b22ee8079f778009e427dea1f7c726e8d84da77b
Dorian MARCHAL <marchal.dorian@gmail.com>
2015-07-02 11:28:01 +0200

    Make sure functions doesn't exist before creating them

--- pr.php
+++ pr.php
@@ -8,116 +8,127 @@
  *
  * @version 1
  */
-function pr($o = '', $calledByPh = false) {
 
-    $enableTerminalColor = true;
+if (!function_exists('pr')) {
 
-    $isCli = isCli();
-    $isAjax = isAjax();
-    $addHtml = !$isCli && (!$isAjax || $calledByPh);
-    $addTerminalColor = $enableTerminalColor && $isCli;
+    function pr($o = '', $calledByPh = false) {
 
-    $valueIsObject = is_object($o);
-    $valueIsArray = is_array($o);
+        $enableTerminalColor = true;
 
-    $label = '';
+        $isCli = isCli();
+        $isAjax = isAjax();
+        $addHtml = !$isCli && (!$isAjax || $calledByPh);
+        $addTerminalColor = $enableTerminalColor && $isCli;
 
-    // Récupération du nom de la variable (via la backtrace : un peu crade mais c'est pour du débuggage)
-    $bt = debug_backtrace();
+        $valueIsObject = is_object($o);
+        $valueIsArray = is_array($o);
 
-    // Si pr est appelée via pa, on récupère le second niveau de la backtrace
-    $btIndex = $calledByPh ? 1 : 0;
+        $label = '';
 
-    $src = file($bt[$btIndex]['file']);
-    $line = $src[$bt[$btIndex]['line'] - 1];
+        // Récupération du nom de la variable (via la backtrace : un peu crade mais c'est pour du débuggage)
+        $bt = debug_backtrace();
 
-    preg_match('#p[rh]\((.*)\)#', $line, $match);
-    $label = $match[1];
+        // Si pr est appelée via pa, on récupère le second niveau de la backtrace
+        $btIndex = $calledByPh ? 1 : 0;
 
-    $firstChar = substr($label, 0, 1);
+        $src = file($bt[$btIndex]['file']);
+        $line = $src[$bt[$btIndex]['line'] - 1];
 
-    // Si la fonction a été appelée sans paramètre, on passe juste une ligne
-    if ($firstChar === false) {
-        // Un <br> en html
+        preg_match('#p[rh]\((.*)\)#', $line, $match);
+        $label = $match[1];
+
+        $firstChar = substr($label, 0, 1);
+
+        // Si la fonction a été appelée sans paramètre, on passe juste une ligne
+        if ($firstChar === false) {
+            // Un <br> en html
+            if ($addHtml) {
+                echo '<br>';
+            }
+            // Un \n ailleurs
+            else {
+                echo "\n";
+            }
+            return;
+        }
+        // Sinon, si on a juste passé une chaîne à la fonction, on l'affiche simplement
+        else if ($firstChar === '\'' ||  $firstChar === '"') {
+            // Un <br> en html
+            if ($addHtml) {
+                echo "$o<br>";
+            }
+            // Un \n ailleurs
+            else {
+                echo "$o\n";
+            }
+            return;
+        }
+
+        // Si on affiche le log dans une page web, on le wrappe dans un <pre>
         if ($addHtml) {
-            echo '<br>';
+            echo '<pre class="debug-pr" style="color:black;font-family:monospace;" >';
+            echo "<strong>$label: </strong>";
         }
-        // Un \n ailleurs
+        // Sinon, si on doit ajouter des couleurs pour le shell
+        else if ($addTerminalColor) {
+            echo "\033[1;34m$label:\033[0m ";
+        }
         else {
-            echo "\n";
+            echo "$label: ";
         }
-        return;
-    }
-    // Sinon, si on a juste passé une chaîne à la fonction, on l'affiche simplement
-    else if ($firstChar === '\'' ||  $firstChar === '"') {
-        // Un <br> en html
+
+        // Dans le cas des tableaux ou des objets, on fait un print_r
+        if ($valueIsObject || $valueIsArray) {
+
+            // Si c'est un tableau, on affiche en plus le nombre d'éléments
+            if ($valueIsArray) {
+                echo '{' . count($o) . '} ';
+            }
+
+            echo print_r($o, true);
+        }
+        // Dans tous les autres cas, on fait un var_dump
+        else {
+            var_dump($o);
+        }
+
         if ($addHtml) {
-            echo "$o<br>";
+            echo '</pre>';
         }
-        // Un \n ailleurs
-        else {
-            echo "$o\n";
-        }
-        return;
-    }
-
-    // Si on affiche le log dans une page web, on le wrappe dans un <pre>
-    if ($addHtml) {
-        echo '<pre class="debug-pr" style="color:black;font-family:monospace;" >';
-        echo "<strong>$label: </strong>";
-    }
-    // Sinon, si on doit ajouter des couleurs pour le shell
-    else if ($addTerminalColor) {
-        echo "\033[1;34m$label:\033[0m ";
-    }
-    else {
-        echo "$label: ";
-    }
-
-    // Dans le cas des tableaux ou des objets, on fait un print_r
-    if ($valueIsObject || $valueIsArray) {
-
-        // Si c'est un tableau, on affiche en plus le nombre d'éléments
-        if ($valueIsArray) {
-            echo '{' . count($o) . '} ';
-        }
-
-        echo print_r($o, true);
-    }
-    // Dans tous les autres cas, on fait un var_dump
-    else {
-        var_dump($o);
-    }
-
-    if ($addHtml) {
-        echo '</pre>';
     }
 }
 
-/**
- * Appelle pr en forçant l'HTML
- */
-function ph($o = '') {
-    pr($o, true);
+if (!function_exists('ph')) {
+
+    /**
+     * Appelle pr en forçant l'HTML
+     */
+    function ph($o = '') {
+        pr($o, true);
+    }
 }
 
-/**
- * Retourne true si le script est exécuté depuis la ligne de commande.
- */
-function isCli() {
-    return php_sapi_name() === 'cli';
+if (!function_exists('isCli')) {
+    /**
+     * Retourne true si le script est exécuté depuis la ligne de commande.
+     */
+    function isCli() {
+        return php_sapi_name() === 'cli';
+    }
 }
 
-/**
- * Retourne true si le script est appelé en AJAX
- * /!\ Cette fonction n'est pas sûre.
- */
-function isAjax() {
+if (!function_exists('isAjax')) {
+    /**
+     * Retourne true si le script est appelé en AJAX
+     * /!\ Cette fonction n'est pas sûre.
+     */
+    function isAjax() {
 
-    $isAjax = !empty($_SERVER['HTTP_X_REQUESTED_WITH'])
-        && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+        $isAjax = !empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
 
-    return $isAjax;
+        return $isAjax;
+    }
 }
 
 ?>
